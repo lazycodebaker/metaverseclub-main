b3a4c5962fe460aa279e147016b01ee9d6fa113c
Anshuman Tiwari <lazycodebaker@outlook.com>
2023-03-07 11:06:58 +0530

    .

---
+++ src/components/TokenCard.tsx
@@ -0,0 +1,6 @@
+
+export const TokenCard: React.FC<{}> = () => {
+    return (        
+        <img alt="content" className="object-cover object-center h-full w-full" src="https://dummyimage.com/1201x501" />
+    );
+};
