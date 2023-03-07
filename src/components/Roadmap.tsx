34dc13a4c1b089b2a81111eb5849ee03f6743c97
Anshuman Tiwari <lazycodebaker@outlook.com>
2023-03-07 13:24:06 +0530

    .

--- src/components/Roadmap.tsx
+++ src/components/Roadmap.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 
 export const Roadmap : React.FC<{}> = () =>{
     return(
-        <div className="w-full h-full sm:px-80">
+        <div className="w-full h-full sm:px-80 px-3">
             <Image src={RoadmapImg} alt="Roadmap" className="w-full h-full object-fit" />          
         </div>
     )
