35ec826b53ab06803e7b7763599e9baabd92e73d
Anshuman Tiwari <lazycodebaker@outlook.com>
2023-03-09 15:21:46 +0530

    .

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -6,9 +6,11 @@ import { Doughnut } from "react-chartjs-2";
 import { Tooltip, ArcElement, Title, Legend, Chart as ChartJS } from "chart.js";
 import { TokenCard } from "./TokenCard";
 
+import Background from 'public/images/background.jpg';
+
 ChartJS.register(ArcElement, Title, Legend, Tooltip);
 
-export const Chart: React.FC<{}> = () => {
+export const Chart: React.FC<{}> = ({}) => {
 
     const data = {
         labels: [
@@ -49,7 +51,10 @@ export const Chart: React.FC<{}> = () => {
     return (
         <section className="text-gray-600 body-font">
             <h1 className='font-bold text-5xl sm:text-7xl text-gray-50 text-center self-center mt-12'>TOKENOMICS</h1>
-            <div className="container px-5 py-24 mx-auto">
+
+            <img src={Background.src} className='w-full -z-10 h-full absolute ' />
+
+            <div className="container px-5 py-24 mx-auto z-50">
                 <div className="flex flex-wrap -mx-4 -mb-10 text-center ">
 
                     <div className="sm:w-3/5 w-full mb-10 px-2">
@@ -58,7 +63,7 @@ export const Chart: React.FC<{}> = () => {
                         </div>
                     </div>
 
-                    <div className="sm:w-2/5 w-full h-full mb-10 px-6">
+                    <div className="sm:w-2/5 w-full h-full mb-10 px-6 z-50">
                         <div className="rounded-lg h-full overflow-hidden">
                             <Doughnut
                                 data={data}
