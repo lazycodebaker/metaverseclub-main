97ee254be578f85582c7577089da1fe4e708600e
Anshuman Tiwari <lazycodebaker@outlook.com>
2023-03-09 12:07:38 +0530

    .

--- src/components/Teams.tsx
+++ src/components/Teams.tsx
@@ -2,7 +2,72 @@
 export const Teams: React.FC<{}> = () => {
     return (
         <div className="flex flex-col items-center justify-center pt-16">
-            <h1 className='font-bold text-5xl sm:text-7xl text-gray-50 text-center self-center mb-12'> TEAM </h1>
+
+            <div className="w-full bg-gray-800">
+                <section className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-12">
+                    <div className="text-center pb-12">
+                        <h1 className="font-bold text-3xl md:text-4xl lg:text-5xl font-heading text-white">
+                            Check our awesome team members
+                        </h1>
+                    </div>
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                        <div className="w-full bg-gray-900 rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center">
+                            <div className="mb-8">
+                                <img className="object-center object-cover rounded-full h-36 w-36" src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" alt="photo" />
+                            </div>
+                            <div className="text-center">
+                                <p className="text-xl text-white font-bold mb-2">Dany Bailey</p>
+                                <p className="text-base text-gray-400 font-normal">Software Engineer</p>
+                            </div>
+                        </div>
+                        <div className="w-full bg-gray-900 rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center">
+                            <div className="mb-8">
+                                <img className="object-center object-cover rounded-full h-36 w-36" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" alt="photo" />
+                            </div>
+                            <div className="text-center">
+                                <p className="text-xl text-white font-bold mb-2">Lucy Carter</p>
+                                <p className="text-base text-gray-400 font-normal">Graphic Designer</p>
+                            </div>
+                        </div>
+                        <div className="w-full bg-gray-900 rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center">
+                            <div className="mb-8">
+                                <img className="object-center object-cover rounded-full h-36 w-36" src="https://images.unsplash.com/photo-1499952127939-9bbf5af6c51c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1176&q=80" alt="photo" />
+                            </div>
+                            <div className="text-center">
+                                <p className="text-xl text-white font-bold mb-2">Jade Bradley</p>
+                                <p className="text-base text-gray-400 font-normal">Dev Ops</p>
+                            </div>
+                        </div>
+                        <div className="w-full bg-gray-900 rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center">
+                            <div className="mb-8">
+                                <img className="object-center object-cover rounded-full h-36 w-36" src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" alt="photo" />
+                            </div>
+                            <div className="text-center">
+                                <p className="text-xl text-white font-bold mb-2">Dany Bailey</p>
+                                <p className="text-base text-gray-400 font-normal">Software Engineer</p>
+                            </div>
+                        </div>
+                        <div className="w-full bg-gray-900 rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center">
+                            <div className="mb-8">
+                                <img className="object-center object-cover rounded-full h-36 w-36" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" alt="photo" />
+                            </div>
+                            <div className="text-center">
+                                <p className="text-xl text-white font-bold mb-2">Lucy Carter</p>
+                                <p className="text-base text-gray-400 font-normal">Graphic Designer</p>
+                            </div>
+                        </div>
+                        <div className="w-full bg-gray-900 rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center">
+                            <div className="mb-8">
+                                <img className="object-center object-cover rounded-full h-36 w-36" src="https://images.unsplash.com/photo-1499952127939-9bbf5af6c51c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1176&q=80" alt="photo" />
+                            </div>
+                            <div className="text-center">
+                                <p className="text-xl text-white font-bold mb-2">Jade Bradley</p>
+                                <p className="text-base text-gray-400 font-normal">Dev Ops</p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
 
         </div>
     )
