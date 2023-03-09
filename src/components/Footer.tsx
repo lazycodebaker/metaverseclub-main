d7eac65c9079588fbfc0a21326d54e4371012b45
Anshuman Tiwari <lazycodebaker@outlook.com>
2023-03-09 17:10:07 +0530

    .

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@
 
 export const Footer: React.FC<{}> = () => {
     return (
-        <div className="flex flex-col items-center justify-center mt-16 py-10 bg-black w-screen border-t-[1px] border-white">
+        <div className="flex flex-col items-center justify-center mt-16 px-7 py-10 bg-black w-screen border-t-[1px] border-white">
             <h1 className="text-white text-md font-bold">
                 Disclaimer: Your investment may go down as well as up in value. Cryptocurrency is not regulated in the UK.
             </h1>
