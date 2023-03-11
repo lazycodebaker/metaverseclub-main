f17d9f28b4b1b8954fff8aaf54841144cc608799
Anshuman <lazycodebaker@outlook.com>
2023-03-11 09:50:17 +0530

    s

--- src/components/Teams.tsx
+++ src/components/Teams.tsx
@@ -1,3 +1,8 @@
+import Image from 'next/image';
+import Shivam from 'public/logos/solid.png';
+import Alok from 'public/logos/certik.png';
+import Anshuman from 'public/logos/solid.png';
+import Arpit from 'public/logos/certik.png';
 
 export const Teams: React.FC<{}> = () => {
     return (
@@ -13,7 +18,7 @@ export const Teams: React.FC<{}> = () => {
                     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
                         <div className="w-full bg-gray-900 rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center">
                             <div className="mb-8">
-                                <img className="object-center object-cover rounded-full h-36 w-36" src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" alt="photo" />
+                                <img className="object-center object-cover rounded-full h-36 w-36" src={Shivam} alt="photo" />
                             </div>
                             <div className="text-center">
                                 <p className="text-xl text-white font-bold mb-2">Dany Bailey</p>
@@ -22,7 +27,7 @@ export const Teams: React.FC<{}> = () => {
                         </div>
                         <div className="w-full bg-gray-900 rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center">
                             <div className="mb-8">
-                                <img className="object-center object-cover rounded-full h-36 w-36" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" alt="photo" />
+                                <img className="object-center object-cover rounded-full h-36 w-36" src={Alok} alt="photo" />
                             </div>
                             <div className="text-center">
                                 <p className="text-xl text-white font-bold mb-2">Lucy Carter</p>
@@ -31,7 +36,7 @@ export const Teams: React.FC<{}> = () => {
                         </div>
                         <div className="w-full bg-gray-900 rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center">
                             <div className="mb-8">
-                                <img className="object-center object-cover rounded-full h-36 w-36" src="https://images.unsplash.com/photo-1499952127939-9bbf5af6c51c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1176&q=80" alt="photo" />
+                                <img className="object-center object-cover rounded-full h-36 w-36" src={Anshuman} alt="photo" />
                             </div>
                             <div className="text-center">
                                 <p className="text-xl text-white font-bold mb-2">Jade Bradley</p>
@@ -40,31 +45,14 @@ export const Teams: React.FC<{}> = () => {
                         </div>
                         <div className="w-full bg-gray-900 rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center">
                             <div className="mb-8">
-                                <img className="object-center object-cover rounded-full h-36 w-36" src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" alt="photo" />
+                                <img className="object-center object-cover rounded-full h-36 w-36" src={Arpit} alt="photo" />
                             </div>
                             <div className="text-center">
                                 <p className="text-xl text-white font-bold mb-2">Dany Bailey</p>
                                 <p className="text-base text-gray-400 font-normal">Software Engineer</p>
                             </div>
                         </div>
-                        <div className="w-full bg-gray-900 rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center">
-                            <div className="mb-8">
-                                <img className="object-center object-cover rounded-full h-36 w-36" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" alt="photo" />
-                            </div>
-                            <div className="text-center">
-                                <p className="text-xl text-white font-bold mb-2">Lucy Carter</p>
-                                <p className="text-base text-gray-400 font-normal">Graphic Designer</p>
-                            </div>
-                        </div>
-                        <div className="w-full bg-gray-900 rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center">
-                            <div className="mb-8">
-                                <img className="object-center object-cover rounded-full h-36 w-36" src="https://images.unsplash.com/photo-1499952127939-9bbf5af6c51c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1176&q=80" alt="photo" />
-                            </div>
-                            <div className="text-center">
-                                <p className="text-xl text-white font-bold mb-2">Jade Bradley</p>
-                                <p className="text-base text-gray-400 font-normal">Dev Ops</p>
-                            </div>
-                        </div>
+                       
                     </div>
                 </section>
             </div>
