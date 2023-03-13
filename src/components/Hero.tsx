5096b164cbfb23b7d28f6a8dffb1b4ecccdd9d62
Anshuman Tiwari <lazycodebaker@outlook.com>
2023-03-13 10:37:38 +0530

    .

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -37,7 +37,7 @@ export const Hero: React.FC = () => {
       <div className="container mx-auto flex px-5 md:flex-row flex-col items-center">
 
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="welcome title-font sm:text-7xl text-4xl mb-10 font-bold text-gray-50">
+          <h1 className="title-font sm:text-7xl text-4xl mb-10 font-bold text-gray-50">
             WELCOME <br /> TO <br /> METAVERSE CLUB
           </h1>
         </div>
