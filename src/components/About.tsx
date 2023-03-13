86f992cfbd5bd2d52b4bdcabf56cf0a528d26351
Anshuman <lazycodebaker@outlook.com>
2023-03-13 09:35:21 +0530

    z

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -25,8 +25,8 @@ export const About: React.FC = () => {
                     </div>
                 </div>
                 <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-    <iframe width="100%" height="315" src="https://www.youtube.com/embed/HwT1dRYtoaY" frameborder="0" allowfullscreen></iframe>
-</div>
+                        <iframe width="100%" height="315" src="https://www.youtube.com/embed/HwT1dRYtoaY" frameborder="0" allowfullscreen></iframe>
+                </div>
 
             </div>
 
