94b84bc89e00c6075ef0b7465132ddeaf59f793c
Anshuman Tiwari <lazycodebaker@outlook.com>
2023-03-13 10:42:30 +0530

    .

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -34,7 +34,7 @@ export const Hero: React.FC = () => {
 
   return (
     <section className="text-gray-50 body-font z-40 mt-24">
-      <div className="container mx-auto flex px-5 md:flex-row flex-col items-center">
+      <div className="container mx-auto flex px-5 py-10 md:flex-row flex-col items-center">
 
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="welcome title-font sm:text-7xl text-4xl mb-10 font-bold text-gray-50">
